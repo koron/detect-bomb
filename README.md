bd6f7a65ce9032b9aaa77bfe1eb8f49baeea18e5
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-09-05 01:03:49 +0900

    first version

---
+++ README.md
@@ -0,0 +1,11 @@
+# detect BOMB
+
+Detects files which have BOMB (byte order mark bytes) as UTF-8 encoded text.
+
+    # Install
+    $ go install github.com/koron/detect-bomb
+
+    # How to run
+    $ cd $GOPATH/github.com/koron/detect-bomb
+    $ detect-bomb
+    BOMB: testdata/bomb.txt
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var (
+	excludes = []string{
+		".jekyll-metadata",
+		".sass-cache",
+		"_site",
+		"tmp",
+	}
+	logger = log.New(ioutil.Discard, "", log.LstdFlags)
+)
+
+func hasBomb(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		logger.Printf("failed to open: %s", err)
+		return false
+	}
+	defer f.Close()
+	br := bufio.NewReader(f)
+	r, _, err := br.ReadRune()
+	if err != nil {
+		logger.Printf("failed to read: %s", err)
+		return false
+	}
+	return r == '\uFEFF'
+}
+
+func main() {
+	verbose := flag.Bool("verbose", false, "verbose messages")
+	flag.Parse()
+	if *verbose {
+		logger = log.New(os.Stderr, "", log.LstdFlags)
+	}
+	ignores := make(map[string]bool)
+	for _, n := range excludes {
+		ignores[n] = true
+	}
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		_, ok := ignores[info.Name()]
+		if ok {
+			logger.Printf("SKIPPED: %s", path)
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if hasBomb(path) {
+			fmt.Printf("BOMB: %s\n", path)
+		}
+		return nil
+	})
+}
